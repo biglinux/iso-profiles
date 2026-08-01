ff126fb7ba02c78d540bc74f5bb61faf18acdf1c
Bruno Gonçalves <bigbruno@gmail.com>
2026-08-01 07:43:25 -0300

    test: advance through BigLinux keyboard setup

--- openqa/tests/boot_menu.pm
+++ openqa/tests/boot_menu.pm
@@ -7,7 +7,8 @@ sub run {
     assert_screen 'biglinux-boot-menu', 120;
     send_key 'ret';
     assert_and_click 'biglinux-live-language', timeout => 300, mousehide => 1;
-    assert_screen 'biglinux-live-desktop', 120;
+    assert_and_click 'biglinux-live-keyboard', timeout => 30, mousehide => 1;
+    assert_screen 'biglinux-live-display', 60;
 }
 
 1;
