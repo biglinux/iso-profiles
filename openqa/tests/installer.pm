246fc4d5a8ae77aaddb5285e8dc6159796f03e7b
Bruno Gonçalves <bigbruno@gmail.com>
2026-08-01 09:06:53 -0300

    test: assert Calamares setup pages

--- openqa/tests/installer.pm
+++ openqa/tests/installer.pm
@@ -3,20 +3,6 @@
 use Mojo::Base 'basetest';
 use testapi;
 
-sub click_at {
-    my ($x, $y) = @_;
-
-    mouse_set $x, $y;
-    mouse_click 'left';
-}
-
-sub capture_reference {
-    my ($title) = @_;
-
-    record_info $title, 'Reference image captured during test authoring';
-    save_screenshot;
-}
-
 sub run {
     send_key 'alt-f2';
     type_string 'calamares-biglinux_polkit --software-render';
@@ -31,16 +17,13 @@ sub run {
     assert_and_click 'biglinux-installer-welcome', timeout => 60, mousehide => 1;
 
     sleep 5;
-    capture_reference 'Calamares location';
-    click_at 855, 706;
+    assert_and_click 'biglinux-installer-location', timeout => 30, mousehide => 1;
 
     sleep 5;
-    capture_reference 'Calamares keyboard';
-    click_at 855, 706;
+    assert_and_click 'biglinux-installer-keyboard', timeout => 30, mousehide => 1;
 
     sleep 5;
-    capture_reference 'Calamares partitions';
-    assert_screen 'biglinux-installer-partitions', 30;
+    assert_screen 'biglinux-installer-partitions', 30, mousehide => 1;
 }
 
 1;
